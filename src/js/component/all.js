dc091ca13317cf19243db3ffec562f8a985c164d
chenchangwen <21108589@qq.com>
2015-04-01 02:38:35 +0800

    Signed-off-by: chenchangwen <21108589@qq.com>

---
+++ src/js/component/all.js
@@ -0,0 +1,36 @@
+﻿require.config({
+    paths: {
+        link: "src/js/component/link",
+        button: "src/js/component/button",
+        regexp: "src/js/component/regexp",
+        countdown: "src/js/component/countdown",
+        componentutils: 'src/js/component/utils'
+    }
+});
+define(["link", "button", "regexp", "countdown"], function (ccwlink, ccwbutton, ccwregexp, ccwcountdown) {
+    var exports = {
+        link: ccwlink,
+        button: ccwbutton,
+        regexp: ccwregexp,
+        countdown:ccwcountdown,
+        show: function (el) {
+            callFn('show',el);
+        },
+        save: function (el) {
+            callFn('save',el);
+        }
+    };
+
+    function callFn(fnname, el) {
+        for (var pfn in exports) {
+            if (pfn !== 'show' && pfn !== 'save') {
+                var fn = exports[pfn][fnname];
+                if (typeof (fn) == "function") {
+                    fn(el);
+                }
+            }
+        }
+    }
+
+    return exports;
+});
---
+++ src/js/global.config.js
@@ -0,0 +1,29 @@
+﻿require.config({
+    baseUrl:'../../',
+    paths: {
+        jquery: 'vendor/jquery.min',
+        Jcrop: 'vendor/Jcrop/jquery.Jcrop.min',
+        "uikit": 'vendor/uikit/js/uikit.min',
+        "spin": 'vendor/spin.min',
+        jqextend: 'src/js/core/jqextend',
+        uikitextend: 'src/js/core/uikitextend',
+        utils: 'src/js/core/utils',
+        'uikit!upload': 'uikit!upload',
+        component: 'src/js/component/all'
+    },
+    shim: {
+        jqextend: ['jquery'],
+        uikitextend: ['jquery'],
+        utils: ['jquery'],
+        "spin": ['jquery'],
+        Jcrop: ['jquery'],
+        "uikit": ['jquery'],
+        "uikit!upload": ['jquery']
+    },
+    config: {
+        "uikit": {
+            "base": "vendor/uikit/js"
+        }
+    },
+    waitSeconds: 200
+});
